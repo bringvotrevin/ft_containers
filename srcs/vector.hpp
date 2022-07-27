9da2f9a09654c806eda18003bee5848c3688c272
dimfrom42 <dim@student.42seoul.kr>
2022-07-27 19:21:30 +0900

    [ADD] enable_if template

--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -4,8 +4,40 @@
 #include <iostream>
 #include <vector>
 
+namespace ft {
+
 template <class T, class Alloc = allocator<T> >
 class  vector
 {
-    
+    public:
+        vector(); //allocator로 생성?
+        vector&    operator=(const vector& other)
+        {
+            if (this != other)
+                *this = other;
+            return (*this);
+        };
+        ~vector();
+
+};
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+// enable_if
+template <bool B, class T = void>
+struct enable_if{};
+
+template<class T>
+struct enable_if<true, T> {typedef T type;};
