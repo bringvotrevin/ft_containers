8ee29339f4c4a38a8cc4d5886a81a6bc3b4f97cf
dimfrom42 <dim@student.42seoul.kr>
2022-07-28 16:48:24 +0900

    [FIX] usign -> typedef

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -7,28 +7,47 @@
 
 namespace ft {
 
+
+
 template <class _T, class _Alloc = std::allocator<_T> >
 class  vector
 {
-    public:
-        /*
-        vector(); //allocator로 생성?
-        vector&    operator=(const vector& other)
-        {
-            if (this != other)
-                *this = other;
-            return (*this);
-        };
-        ~vector();
-        */
+   public:
+      typedef _T                                         value_type;
+      typedef _Alloc                                     allocator_type;
+      typedef typename allocator_type::reference         reference;
+      typedef typename allocator_type::const_reference   const_reference;
+      typedef typename allocator_type::pointer           pointer;
+      typedef typename allocator_type::const_pointer     cons_pointer;
 
-       using value_type = _T;
-       using allocator_type = _Alloc;
-       using reference = _T&;
-       using const_reference = const T&;
-       using pointer = _T*; // ??;
-       using const_pointer = const _T*;
-       using iterator = ;
+      typedef iterator; //
+      typedef const_iterator; //
+      typedef reverse_iterator; //
+      typedef const_reverse_iterator; //
+      typedef difference_type; //
+      typedef size_type; //
+
+
+
+
+      //  using value_type = _T;
+      //  using allocator_type = _Alloc;
+      //  using reference = _T&;
+      //  using reference = allocator_type::reference;
+      //   using const_reference = const T&;
+      //  using const_reference = allocator_type::const_reference;
+      //   using pointer = _T*; // ??;
+      //  using pointer = allocator_type::pointer;
+      //   using const_pointer = const _T*;
+      //  using const_pointer = allocator_type::const_pointer;
+
+      //  using iterator = ;
+      //  using const_iterator = ;
+      //  using reverse_iterator = ;
+      //  using const_reverse_iterator = ;
+
+      //  using difference_type = ;
+      //  using size_type = ;
 
 
 
@@ -52,12 +71,8 @@ class _V_iterator // const v itorator 상속받음
 
 
 
-
-
-
-
-
 }
+
 
 
 
