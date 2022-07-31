0fed0dbebd9b3881a6cd7d8eed6d3dba411bc526
dim <dim@student.42seoul.kr>
2022-08-01 05:23:37 +0900

    [FEAT]: vector capacity and element access member function declaration

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -12,13 +12,14 @@ namespace ft {
 template <class _T, class _Alloc = std::allocator<_T> >
 class  vector
 {
-	public:
+	private:
 		typedef _T											value_type;
 		typedef _Alloc										allocator_type;
 		typedef typename allocator_type::reference			reference;
 		typedef typename allocator_type::const_reference	const_reference;
 		typedef typename allocator_type::pointer			pointer;
-		typedef typename allocator_type::const_pointer		cons_pointer		
+		typedef typename allocator_type::const_pointer		const_pointer;
+
 		typedef iterator; //
 		typedef const_iterator; //
 		typedef reverse_iterator; //
@@ -26,15 +27,54 @@ class  vector
 		typedef typename allocator_type::difference_type	difference_type;
 		typedef typename allocator_type::size_type			size_type;
 
-		explicit vector(const allocator_type&);
+		// Member functions
+
+		// constructor
+		explicit vector(const allocator_type& alloc)
+		: _Alloc(alloc), size(0), capacity(1)
+		{
+			// TODO vector allocator
+		};
 		explicit vector(size_type n, const value_type& value, const allocator_type& = allocator_type());
 		template <class InputIterator>
 			vector(InputIterator first, InpusIterator last, const allocator_type& = allocator_type());
 		vector(const vector& v);
-
+		// destructor
 		~vector();
+		// operator =
+		vector&		operator=(const vector& x);
 
-		vector& operator=(const vector& x);
+		// Iterators:
+
+
+		// Capacity :
+		size_type	size() const;
+		size_type	max_size() const;
+		void		resize(size_type n, value_type val = value_type());
+		size_type	capacity() const;
+		bool		empty() const;
+		void		reserve(size_type n);
+
+		// Element access
+		reference		operator[](size_type n);
+		const_reference operator[](size_type n) const;
+		reference		at(size_type n);
+		const_reference	at(size_type n) const;
+		reference		front();
+		const_reference	front() const;
+		reference		back();
+		const_reference	back() const;
+
+		//	Modifiers
+		template <class InputIterator>
+		void	assign(InputIterator fist, InputIterator last); //range
+		void	assign(size_type n, const value_type& val); //fill
+		// TODO push_back, pop_back, insert, erase, swap, clear, emplace, emplace_back
+
+		//	Allocator
+		// TODO get_allocator
+
+
 
 
 };
--- includes/vector_iterator.hpp
+++ includes/vector_iterator.hpp
@@ -6,6 +6,12 @@
 
 namespace ft {
 
+template <class Category, class T, class Distance = ptrdiff_t,
+			class Pointer = T*, class Reference = T&>
+class iterator
+{
+	
+};
 
 }
 
